336c1cd7a5e219bcc6d5f6c0efe8911ecf5bd3b7
vagrant <vagrant@localhost.localdomain>
2019-10-07 11:50:22 +0000

    静的ページを作った

---
+++ spec/features/static_page_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+RSpec.feature "StaticPages", type: :feature do
+
+  describe "Home page" do
+    before do
+      visit root_path   # 名前付きルートを使用
+    end
+#
+#    # HomeページにStaticPages#homeと表示されていること
+    it "should have the content 'Books App'" do
+      expect(page).to have_content "Books App"
+    end
+
+    # タイトルが正しく表示されていること
+    it "should have the right title" do
+      expect(page).to have_title full_title('')
+    end
+
+  end
+end
