765e18cd04ae04617c3c4dcfd5fb6e49b2f48a06
vagrant <vagrant@localhost.localdomain>
2019-10-08 05:27:23 +0000

    user registration

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,3 +6,9 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
+
+# ログインする
+def sign_in_as(user)
+  post login_path, params: { session: { email: user.email,
+                                      password: user.password } }
+end
